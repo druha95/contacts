a6ce8816c7f4c2a204c9691ef4df275fb2dd7044
Andrew Medvedsky <a.medvedskyi@bytesarea.com>
2015-11-09 23:43:24 +0200

    added hotmail

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -9,9 +9,34 @@
  */
 angular.module('izzlyApp')
   .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    WL.init({
+      client_id: '0000000040173F6C',
+      redirect_uri: 'http://localhost:9000/',
+      scope: ["wl.basic", "wl.contacts_emails"],
+      response_type: "token"
+    });
+
+    WL.login({
+      scope: ["wl.basic", "wl.contacts_emails"]
+    }).then(function (response)
+      {
+        WL.api({
+          path: "me/contacts",
+          method: "GET"
+        }).then(
+          function (response) {
+            debugger;
+            //your response data with contacts
+            console.log(response.data);
+          },
+          function (responseFailed) {
+            //console.log(responseFailed);
+          }
+        );
+
+      },
+      function (responseFailed)
+      {
+        //console.log("Error signing in: " + responseFailed.error_description);
+      });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,31 +9,37 @@
  * from gmail
  */
 angular.module('izzlyApp')
-  .controller('GmailContacts',['$scope', function ($scope) {
+  .controller('GmailContacts',['$scope','$window', '$http','$timeout',
+              function ($scope, $window, $http, $timeout) {
 
     $scope.applicationData = {};
     $scope.applicationData.clientId = '846336466595-nucqeu94903ddb46iioqnen02u0kefrs.apps.googleusercontent.com';
     $scope.applicationData.apiKey = 'AIzaSyCpRkWJnLzOV1arohiFtn0u4EULkC2acQ8';
     $scope.applicationData.scopes = 'https://www.google.com/m8/feeds';
-    $scope.gapi = angular.copy(window.gapi);
+    $scope.gapi = $window.gapi;
 
     $scope.contactsList = [];
 
     $scope.gapi.client.setApiKey($scope.applicationData.apiKey);
-    window.setTimeout(checkAuth,100);
 
 
-    function checkAuth() {
-      $scope.gapi.auth.authorize({client_id: $scope.applicationData.clientId, scope: $scope.applicationData.scopes, immediate: false}, handleAuthResult);
-    }
+    $scope.checkAuth = function() {
+      $scope.gapi.auth.authorize({client_id: $scope.applicationData.clientId, scope: $scope.applicationData.scopes, immediate: false}, $scope.handleAuthResult);
+    };
 
-    function handleAuthResult(authResult) {
+
+    $scope.handleAuthResult = function (authResult) {
       if (authResult && !authResult.error) {
-        $.get("https://www.google.com/m8/feeds/contacts/default/full?alt=json&access_token=" + authResult.access_token + "&max-results=700&v=3.0",
+        var url = "https://www.google.com/m8/feeds/contacts/default/full?alt=json&access_token=" + authResult.access_token + "&max-results=700&v=3.0";
+        $http.get(url).then(
           function(response){
+            $scope.contactsList = response;
+            debugger;
+          }, function(response){
             $scope.contactsList = response;
             debugger;
           });
       }
-    }
+    };
+    $scope.checkAuth();
   }]);
